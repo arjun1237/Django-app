885527c3c4de06ac6346e37886be9367c2a58f12
arjun.blr1237 <arjun.blr1237@gmail.com>
2020-04-09 15:57:13 +0530

    added models and also worked on templates

--- pycrm/accounts/views.py
+++ pycrm/accounts/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def home(request):
-	return render(request, 'accounts/dashboard.html')
+	orders = Order.objects.all()
+	orders_top5 = orders.order_by('status')[:5]
+	customers = Customer.objects.all()
+	total_cust = customers.count()
+	total_ord = orders.count()
+	delivered = orders.filter(status='Delivered').count()
+	pending = orders.filter(status='Pending').count()
+
+
+	context = {'orders': orders_top5, 'customers': customers,
+	'total_ord': total_ord, 'total_cust': total_cust,
+	'delivered': delivered, 'pending': pending
+	}
+	return render(request, 'accounts/dashboard.html', context)
 
 def products(request):
-	return render(request, 'accounts/products.html')
+	products = Product.objects.all()
+	return render(request, 'accounts/products.html', {'products' : products})
 
 def customer(request):
 	return render(request, 'accounts/customer.html')
